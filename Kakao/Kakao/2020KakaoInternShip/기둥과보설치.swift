e0bf2efb1545ea8f6b9af177e9d4b0a7b2e8f316
pkh <toby369369@naver.com>
2021-01-28 14:39:14 +0900

    add 2018kakao 기둥과보설치

--- Kakao/Kakao/2020KakaoInternShip/기둥과보설치.swift
+++ Kakao/Kakao/2020KakaoInternShip/기둥과보설치.swift
@@ -9,59 +9,106 @@ import Foundation
 
 class 기둥과보설치 {
     func solution(_ n:Int, _ build_frame:[[Int]]) -> [[Int]] {
-        // [[1,0,0,1],[1,1,1,1],[2,1,0,1],[2,2,1,1],[5,0,0,1],[5,1,0,1],[4,2,1,1],[3,2,1,1]]
-        var array = Array(repeating: Array(repeating: -1, count: n+1), count: n+1)
+        // default: 0, 기둥: 1, 보: 2
+        var array = Array(repeating: Array(repeating: 0, count: n+3), count: n+3)
         
         for program in build_frame {
-            let x = program[0]
-            let y = program[1]
+            let x = program[0] + 1
+            let y = program[1] + 1
             let type = program[2]
             let doSomething = program[3]
             
             if doSomething == 0 {
                 // 제거
                 let temp = array[y][x]
-                array[y][x] = -1
-                if !isCorrectPosition(array, type, doSomething, x, y) {
+                if type == 0 {
+                    array[y][x] -= 1
+                } else {
+                    array[y][x] -= 2
+                }
+                
+                if !removeStructure(array, n) {
                     array[y][x] = temp
                 }
             } else {
                 // 설치
                 if type == 0 {
-                    array[y][x] = 0
-                    
+                    // 기둥
+                    if isCorrectHorizontalBar(array, y, x) {
+                        array[y][x] += 1
+                    }
                 } else {
-                    array[y][x] = 1
+                    // 보
+                    if isCorrectVerticalBar(array, y, x) {
+                        array[y][x] += 2
+                    }
                 }
-                if !isCorrectPosition(array, type, doSomething, x, y) {
-                    array[y][x] = -1
-                }
-                
             }
         }
         
         var result = [[Int]]()
         
-        for i in 0..<array.count {
-            for j in 0..<array[i].count {
-                if array[i][j] == 0 || array[i][j] == 1 {
-                    result.append([j, i, array[i][j]])
+        for i in 1...n+1 {
+            for j in 1...n+1 {
+                // 기둥
+                if array[i][j] == 1 {
+                    result.append([j-1, i-1, 0])
+                } else if array[i][j] == 2  {
+                    // 보
+                    result.append([j-1, i-1, 1])
+                } else if array[i][j] == 3 {
+                    // 기둥&보
+                    result.append([j-1, i-1, 0])
+                    result.append([j-1, i-1, 1])
                 }
             }
         }
-        print(array)
         
         result = result.sorted(by: {
             if $0[0] == $1[0] {
                 return $0[1] < $1[1]
-            } else if $0[0] == $1[0] && $0[1] == $1[1] {
+            } else if ($0[0] == $1[0]) && ($0[1] == $1[1]) {
                 return $0[2] < $1[2]
             }
             return $0[0] < $1[0]
         })
         
-//        print(result)
+        print(result)
+        
         return result
+    }
+    
+    // 기둥
+    func isCorrectHorizontalBar(_ array: [[Int]],_ x: Int,_ y: Int) -> Bool {
+        if x == 1 { return true }
+        let isVerticalBar = array[x][y] == 2 || array[x][y] == 3
+        let isOnVerticalBar = array[x][y-1] == 2 || array[x][y-1] == 3
+        let isOnAnotherBar = array[x-1][y] == 1 || array[x-1][y] == 3
+        return isVerticalBar || isOnVerticalBar || isOnAnotherBar
+    }
+    // 보
+    func isCorrectVerticalBar(_ array: [[Int]], _ x: Int, _ y: Int) -> Bool {
+        let isOnBar = (array[x-1][y] == 1 || array[x-1][y] == 3) || (array[x-1][y+1] == 1 || array[x-1][y+1] == 3)
+        let isBetweenBar = (array[x][y-1] == 2 || array[x][y-1] == 3) && (array[x][y+1] == 2 || array[x][y+1] == 3)
+        return isOnBar || isBetweenBar
+    }
+    // 제거
+    func removeStructure(_ array: [[Int]],_ n: Int) -> Bool {
+        for i in 1...n+1 {
+            for j in 1...n+1 {
+                if array[i][j] == 1 || array[i][j] == 3 {
+                    if !isCorrectHorizontalBar(array, i, j) {
+                        return false
+                    }
+                }
+                if array[i][j] == 2 || array[i][j] == 3 {
+                    if !isCorrectVerticalBar(array, i, j) {
+                        return false
+                    }
+                }
+            }
+        }
+        return true
     }
     
     func isCorrectPosition(_ array: [[Int]], _ type: Int, _ do: Int,_ x: Int,_ y: Int) -> Bool {
