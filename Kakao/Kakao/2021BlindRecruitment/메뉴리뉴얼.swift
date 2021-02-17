19a7d038813466d9179fe754c69bb4d33648bfd6
park.kyoonho <toby369369@naver.com>
2021-02-17 15:45:20 +0900

    add Kakao 신규아이디추천

--- Kakao/Kakao/2021BlindRecruitment/메뉴리뉴얼.swift
+++ Kakao/Kakao/2021BlindRecruitment/메뉴리뉴얼.swift
@@ -12,7 +12,6 @@ class 메뉴리뉴얼 {
     var dic = [String:Int]()
     
     func solution(_ orders:[String], _ course:[Int]) -> [String] {
-        // 최소 2명이상 손님에게 주문한 메뉴만 구성
         // ["ABCFG", "AC", "CDE", "ACDE", "BCFG ", "ACDEH"]
         // ["ABCDE", "AB", "CD", "ADE", "XYZ", "XYZ", "ACD"]
         // ["XYZ", "XWY", "WXA"]
@@ -20,15 +19,17 @@ class 메뉴리뉴얼 {
         let sorted = orders.sorted(by: { $0.count < $1.count })
         var result = [String]()
         
+        // 주어진 course(2,3,4) 대로 조합 생성
         for combination in course {
-            // 2, 3, 4
             for menus in sorted {
                 let menu = menus.map{ String($0) }
                 makeCombination(0, menu, combination, [])
             }
-            print(dic)
+            
             if !dic.isEmpty {
+                // 가장 많이 주문한 요리의 수
                 let maxCount = dic.map{ $0.value }.max()!
+                // 최소 2번 이상 손님이 주문한 경우만 체크
                 if maxCount >= 2 {
                     let filterd = dic.filter{ $0.value == maxCount }.map{ $0.key }
                     result.append(contentsOf: filterd)
@@ -42,6 +43,7 @@ class 메뉴리뉴얼 {
     
     func makeCombination(_ index: Int, _ array: [String], _ shouldSelect: Int,_ selected: [String]) {
         if shouldSelect == 0 {
+            // 한번 정렬해서 겹치는 경우 체크: AB = BA 같은경우로 봐야함
             let joined = selected.map{ String($0) }.sorted(by: <).joined()
             if let value = dic[joined] {
                 dic[joined] = value + 1
--- Kakao/Kakao/2021BlindRecruitment/신규아이디추천.swift
+++ Kakao/Kakao/2021BlindRecruitment/신규아이디추천.swift
@@ -8,6 +8,51 @@
 import Foundation
 
 class 신규아이디추천 {
+    
+    func solution2(_ new_id:String) -> String {
+        
+        // step1.
+        let lower: String = new_id.lowercased()
+        
+        // step2.
+        let array_id: [Character] = Array(lower)
+        var new_id: String = ""
+        for t in array_id {
+            if t.isNumber || t.isLetter || "-_.".contains(t) {
+                new_id += String(t)
+            }
+        }
+        
+        // step3.
+        while new_id.contains("..") {
+            new_id = new_id.replacingOccurrences(of: "..", with: ".")
+        }
+        
+        // step4.
+        new_id = new_id.trimmingCharacters(in: ["."])
+        
+        
+        // step5.
+        if new_id.isEmpty {
+            new_id = "a"
+        }
+        
+        // step6.
+        if new_id.count > 15 {
+            let start = new_id.startIndex
+            let end = new_id.index(new_id.startIndex, offsetBy: 14)
+            new_id = String(new_id[start...end]).trimmingCharacters(in: ["."])
+        }
+        
+        // step7.
+        while new_id.count < 3 {
+            new_id += String(new_id[new_id.index(before: new_id.endIndex)])
+        }
+        
+        return new_id
+    }
+    
+    
     func solution(_ new_id:String) -> String {
         
         // step1.
@@ -74,7 +119,7 @@ class 신규아이디추천 {
                 }
             }
         }
-//        print(newID)
+        //        print(newID)
         return newID
     }
 }
