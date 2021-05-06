691977aeee42a6d62863df9aa2622181f0496a21
pkh <toby369369@naver.com>
2021-05-06 20:54:11 +0900

    numIdenticalPairs

--- LeetCode/LeetCode/Array/Array.swift
+++ LeetCode/LeetCode/Array/Array.swift
@@ -9,6 +9,19 @@ import Foundation
 
 class _Array {
     
+    func numIdenticalPairs(_ nums: [Int]) -> Int {
+        var count = 0
+        for i in 0..<nums.count {
+            for j in i+1..<nums.count {
+                if nums[i] == nums[j] && i < j {
+                    count += 1
+                }
+            }
+        }
+        
+        return count
+    }
+    
     func checkPossibility(_ nums: [Int]) -> Bool {
         
         //
