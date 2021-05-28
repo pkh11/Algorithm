b345daf3b938f181718fb1013bc54e632b880b50
park.kyoonho <park.kyoonho@joins.com>
2021-05-28 10:08:19 +0900

    add topKfrequent

--- LeetCode/LeetCode/Graph/Graph.swift
+++ LeetCode/LeetCode/Graph/Graph.swift
@@ -8,6 +8,22 @@
 import Foundation
 
 class Graph {
+    
+    func topKFrequent(_ nums: [Int], _ k: Int) -> [Int] {
+        var frequents = [Int:Int]()
+        
+        nums.forEach {
+            if let count = frequents[$0] {
+                frequents[$0] = count + 1
+            } else {
+                frequents[$0] = 1
+            }
+        }
+        
+        let sorted = frequents.sorted { $0.value > $1.value }
+        return sorted[0..<k].map{ $0.key }
+    }
+    
     func findKthLargest(_ nums: [Int], _ k: Int) -> Int {
         let nums = nums.sorted(by: >)
         return nums[k-1]
